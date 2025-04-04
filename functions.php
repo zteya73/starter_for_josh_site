ed3b2099c5ecac1bc4bc346d8b94f7561bebdada
zteya73 <teya4250@gmail.com>
2025-04-04 21:48:08 +0800

    update

--- functions.php
+++ functions.php
@@ -2,10 +2,16 @@
 function load_my_scripts() {
     wp_enqueue_style( 'bootstrap_css', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css');
     wp_enqueue_style( 'fontawesome_css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
-  wp_enqueue_script( 'boot1','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js', array('jquery'));
+    wp_enqueue_script( 'boot1', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js', array('jquery'));
   wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js', array('jquery'));
   wp_enqueue_script( 'boot3','https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js', array('jquery'));
- 
+
+  wp_enqueue_script('gsap-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js', array(), false, true);
+  wp_enqueue_script('gsap-js-scroll', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/ScrollTrigger.min.js', array('gsap-js'), false, true);
+  wp_enqueue_script('gsap-js-custom', get_stylesheet_directory_uri() . '/script1.js', array('gsap-js', 'gsap-js-scroll'), false, true);
+  
+  
+  
   wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
 
