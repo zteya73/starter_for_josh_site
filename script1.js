c901b16097224d277823bad2caa8ac9ee4345335
zteya73 <teya4250@gmail.com>
2025-04-04 23:14:22 +0800

    update

--- script1.js
+++ script1.js
@@ -1,28 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Create the GSAP timeline with ScrollTrigger
-gsap.timeline({
+gsap.timeline().from('.navbar', { scale: 0, duration: 2 })
+  .from('.navbar-brand', { opacity: 0, scale: 0, ease: "back" })
+  .from('.tag', { opacity: 0, y: 10, ease: "back" });
+
+const tl = gsap.timeline({
   scrollTrigger: {
-    trigger: '.about',
-    markers: true, // For debugging only
+    trigger: '.aboutbg',
+    markers: true,
     start: "bottom 90%",
     end: "top 30%",
     scrub: 3,
   }
-})
-  // Navbar animation
-  .from('.navbar', { scale: 0, duration: 2 })
-  .from('.navbar-brand', { opacity: 0, scale: 0, ease: "back" })
-  .from('.tag', { opacity: 0, y: 10, ease: "back" })
+});
 
-  // Scroll-triggered animations
-  .from('.about-text', {
-    duration: 1,
-    y: 250,
-    opacity: 0,
-  })
-  .from('.blocker', {
-    duration: 1,
-    x: 250,
-    opacity: 0,
-  });
+tl.from('.about-text', {
+  duration: 1,
+  y: 250,
+  opacity: 0,
+}).from('.blocker', {
+  duration: 1,
+  x: 250,
+  opacity: 0,
+});
